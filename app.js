c674ed2b10b810acf104b0e36224928e235c79aa
Pezu-git <artem_nikolaev69@mail.ru>
2021-07-02 21:04:44 +0300

    fix serv

--- app.js
+++ app.js
@@ -1,30 +1,29 @@
-
 const Koa = require('koa');
-const uidGenerator = require('node-unique-id-generator')
-const cors = require('koa-cors');
+const cors = require('@koa/cors');
 const koaBody = require('koa-body');
+const idGenerator = require('node-unique-id-generator');
 
-const app = new Koa;
+const app = new Koa();
 
 app.use(cors());
 app.use(koaBody({urlencoded: true}))
 
 const tickets = [{
-  id: uidGenerator.generateUniqueId(),
+  id: idGenerator.generateGUID(),
   name: 'Поменять краску в принтере, ком. 404',
   description: 'Принтер HP LJ-1210, картриджи на складе',
   status: false,
   created: Date.now()
 },
   {
-    id: uidGenerator.generateUniqueId(),
+    id: idGenerator.generateGUID(),
     name: 'Переустановить Windows, PC-Hall24',
     description: '',
     status: false,
     created: Date.now()
   },
   {
-    id: uidGenerator.generateUniqueId(),
+    id: idGenerator.generateGUID(),
     name: 'Установить обновление KB-31642dv3875',
     description: 'Вышло критическое обновление для Windows',
     status: false,
@@ -33,9 +32,6 @@ const tickets = [{
 ];
 
 app.use(async ctx => {
-  ctx.response.set({
-    'Access-Control-Allow-Origin': '*',
-  })
   const {method, id} = ctx.request.query;
   switch (method) {
     case 'allTickets':
@@ -47,7 +43,7 @@ app.use(async ctx => {
     case 'createTicket':
       const createData = JSON.parse(ctx.request.body)
       const newTicket = {
-        id: uidGenerator.generateUniqueId(),
+        id: idGenerator.generateGUID(),
         name: createData.name,
         status: false,
         description: createData.description || '',
